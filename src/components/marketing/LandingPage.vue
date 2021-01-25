d64831ea4795b2b919004a859cddc079b6687330
Dungnv <you@example.com>
2021-01-26 02:11:40 +0700

    update base

---
+++ src/components/marketing/LandingPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="page-wrapper container ">
+    Todo List Example
+    <div class="box-todo-list">
+      <TodoList></TodoList>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import TodoList from "../TodoList";
+
+export default {
+  name: 'landing-page',
+  components: {
+    TodoList
+  },
+  data() {
+    return {
+      TodoList
+    }
+  },
+  methods: {},
+  created() {
+  }
+  , mounted() {
+
+  }
+}
+
+</script>
+
+
+<style lang="scss" scoped>
+.box-todo-list{
+}
+
+</style>
